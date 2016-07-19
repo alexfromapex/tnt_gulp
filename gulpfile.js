f29715f3f341c613725ffa58f6ca53ce7d1fb0aa
Jake Lundberg <wraith@wraithcode.com>
2016-07-19 11:37:06 -0400

    updated gulpfile.js to work with build tool - created new tasks: (syncHtmlToJsCss) to handle updating the dev js and css files when the html file is updated, (syncJsCssToHtml) to handle updated the dev html file when the js and/or css files are updated - also installed chalk to the project

--- gulpfile.js
+++ gulpfile.js
@@ -20,31 +20,8 @@ var config      = argv.config ? require('./' + argv.config) : require('./config'
 var jsFile      = config.jsFile ? config.jsFile.split('.js')[0] + '.min.js' : false;
 var cssFile     = config.cssFile ? config.cssFile.split('.css')[0] + '.min.css' : false;
 var replace     = require('gulp-replace');
+var chalk       = require('chalk');
 
-
-
-/***
-  [COMPLETE] add html file to config file
-
-  [COMPLETE] *** in build tool, need to set delimeter when platform is identified
-  [COMPLETE] *** add build directory within each challenger directory
-
-  [COMPLETE] changed default value of preserveMinifiedFiles to true
-
-  when js/css file is modified
-    write minified files for each
-    prepend + append both minified and non-minified code with script/style tags
-    concat js and css
-    write non-minified code to dev html file
-    write minified code to build/html file
-
-  when html file is modified
-    extract js and css code from html file (make sure to remove script/style tags)
-    write js to dev js file
-    write css to dev css file
-    minification will be handled in the js/css changes code...
-
-***/
 
 
 
@@ -56,6 +33,7 @@ gulp.task('minifyCSS', function() {
       .pipe(gulp.dest(`./${config.directory}`)).on('end', function() {
         prependFile.sync(cssFile, '<style>');
         fs.appendFileSync(cssFile, '</style>');
+        fs.renameSync(cssFile, cssFile.split('/')[0] + '/build/' + cssFile.split('/')[1]);
       });
   }
 });
@@ -73,9 +51,120 @@ gulp.task('minifyJS', function() {
       .pipe(gulp.dest(`./${config.directory}`)).on('end', function() {
         prependFile.sync(jsFile, '<script>');
         fs.appendFileSync(jsFile, '</script>');
+        fs.renameSync(jsFile, jsFile.split('/')[0] + '/build/' + jsFile.split('/')[1]);
       });
   }
 });
+
+gulp.task('syncHtmlToJsCss', function() {
+  //read html file content
+  try {
+    var htmlContent = fs.readFileSync(config.htmlFile, 'utf8');
+  } catch(err) {
+    console.log(chalk.red('[ERROR] - there was an error reading from dev html file...\n'));
+    console.log(chalk.red(err));
+    return false;
+  }
+
+  //extract js from html content and remove script tags
+  var htmlJs = htmlContent.substr(htmlContent.indexOf('<script type="text/javascript">'), (htmlContent.indexOf('</script>') - htmlContent.indexOf('<script type="text/javascript">')));
+  htmlJs = htmlJs.replace('<script type="text/javascript">', '');
+
+  //read existing js file contents
+  try {
+    var jsFileContents = fs.readFileSync(config.jsFile, 'utf8');
+  } catch(err) {
+    console.log(chalk.red('[ERROR] - there was an error reading from dev js file...\n'));
+    console.log(chalk.red(err));
+    return false;
+  }
+
+  //if the extracted js from html is not the same as the existing
+  //js file contents, write the html js to the js file
+  if(htmlJs !== jsFileContents) {
+    try {
+      fs.writeFileSync(config.jsFile, htmlJs, 'utf8');
+    } catch(err) {
+      console.log(chalk.red('[ERROR] - there was an error writing to dev js file...\n'));
+      console.log(chalk.red(err));
+      return false;
+    }
+  }
+
+  //extract css from html content and remove style tags
+  var htmlCss = htmlContent.substr(htmlContent.indexOf('<style class="bbiStyleSheet">'), (htmlContent.indexOf('</style>') - htmlContent.indexOf('<style class="bbiStyleSheet">')));
+  htmlCss = htmlCss.replace('<style class="bbiStyleSheet">', '');
+
+  //read existing css file contents
+  try {
+    var cssFileContents = fs.readFileSync(config.cssFile, 'utf8');
+  } catch(err) {
+    console.log(chalk.red('[ERROR] - there was an error reading from dev css file...\n'));
+    console.log(chalk.red(err));
+    return false;
+  }
+
+  //if the extracted css from html is not the same as the existing
+  //css file contents, write the html css to the css file
+  if(htmlCss !== cssFileContents) {
+    try {
+      fs.writeFileSync(config.cssFile, htmlCss, 'utf8');
+      return true;
+    } catch(err) {
+      console.log(chalk.red('[ERROR] - there was an error writing to dev css file...\n'));
+      console.log(chalk.red(err));
+      return false;
+    }
+  }
+});
+
+gulp.task('syncJsCssToHtml', function() {
+  try {
+    //append/prepend style tags
+    var cssContent = '<style class="bbiStyleSheet">' + fs.readFileSync(config.cssFile, 'utf8') + '</style>';
+  } catch(err) {
+    console.log(chalk.red('[ERROR] - there was an error reading from the dev css file...\n'));
+    console.log(chalk.red(err));
+    return false;
+  }
+
+  try {
+    //append/prepend script tags
+    var jsContent = '<script type="text/javascript">' + fs.readFileSync(config.jsFile, 'utf8') + '</script>';
+  } catch(err) {
+    console.log(chalk.red('[ERROR] - there was an error reading from the dev js file...\n'));
+    console.log(chalk.red(err));
+    return false;
+  }
+
+  //concat js and css
+  var jsCssConcat = cssContent + '\n\n' + jsContent;
+
+  //read contents from dev html file
+  try {
+    var htmlContent = fs.readFileSync(config.htmlFile, 'utf8');
+  } catch(err) {
+    console.log(chalk.red('[ERROR] - there was an error reading from the dev html file...\n'));
+    console.log(chalk.red(err));
+    return false;
+  }
+
+  //if the concatenated css and js is not the same as the dev
+  //html file content, write concatenated code to html file
+  if(jsCssConcat !== htmlContent) {
+    try {
+      fs.writeFileSync(config.htmlFile, jsCssConcat, 'utf8');
+      return true;
+    } catch(err) {
+      console.log(chalk.red('[ERROR] - there was an error writing to the dev html file...\n'));
+      console.log(chalk.red(err));
+      return false;
+    }
+  }
+});
+
+gulp.task('htmlChange', ['syncHtmlToJsCss', 'concat'], function() {});
+gulp.task('jsCssChange', ['syncJsCssToHtml', 'concat'], function() {})
 
 gulp.task('concat', ['minifyCSS', 'minifyJS'], function() {
   return gulp.src(`./${config.directory}${config.buildDirectory}*.min.*`)
@@ -86,9 +175,9 @@ gulp.task('concat', ['minifyCSS', 'minifyJS'], function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(config.cssFile, ['concat']);
-  gulp.watch(config.jsFile, ['concat']);
-  //gulp.watch(config.htmlFile, ['concat']);
+  gulp.watch(config.cssFile, ['jsCssChange']);
+  gulp.watch(config.jsFile, ['jsCssChange']);
+  gulp.watch(config.htmlFile, ['htmlChange']);
 });
 
 gulp.task('default', ['watch']);
